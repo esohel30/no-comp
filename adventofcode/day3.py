728e818dd530f583428867623af3656c3a09d5d9
esohel30 <esohel30@stuy.edu>
2024-03-16 02:01:17 -0400

    solved day 3

--- adventofcode/day3.py
+++ adventofcode/day3.py
@@ -4,6 +4,9 @@ with open('day3.txt', 'r') as file:
     for line in file: 
         map.append(line.strip())
 
+row_len = len(map)
+col_len = len(map[0])
+
 def identity(c): 
     if c == '.': 
         return 0 # if dot 
@@ -33,7 +36,38 @@ def isPartNum(cords):
         else: 
             if identity(map[pair[0]][pair[1]]) == 2: 
                 return True 
-    
+            
     return False 
 
 
+def numLen(i, j): 
+    length = 1
+    while j < col_len - 1 and identity(map[i][j + 1]) == 1: 
+        length += 1
+        j += 1
+    
+    return length 
+
+total = 0 
+
+for i in range(len(map)): 
+    j = 0 
+    while j < col_len: 
+        if identity(map[i][j]) == 1: 
+            length = numLen(i, j)
+            if length == 3: 
+                if isPartNum(where_to_check(i, j)) or isPartNum(where_to_check(i, j + 1)) or isPartNum(where_to_check(i, j + 2)): 
+                    total += (100 * int(map[i][j]) + 10 * int(map[i][j + 1]) + int(map[i][j + 2]))
+            elif length == 2:
+                if isPartNum(where_to_check(i, j)) or  isPartNum(where_to_check(i, j + 1)):
+                    total += (10 * int(map[i][j]) + int(map[i][j + 1]))
+            else: 
+                if isPartNum(where_to_check(i, j)): 
+                    total += (int(map[i][j]))
+
+            j = j + length
+
+        else: 
+            j += 1 
+         
+print(total)
