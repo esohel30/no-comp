83b00710b0be601f085f805b284b6b98fa228135
esohel30 <eric.sohel7@gmail.com>
2024-03-19 04:42:20 -0400

    added another solution that works

--- leetcode/powerset.py
+++ leetcode/powerset.py
@@ -6,16 +6,16 @@ storage = []
 def power_set(nums, index, temp):
     
     if index == len(nums):
-        storage.append(temp[:]) # why copy ? 
+        storage.append(temp) # why copy ? 
         return 
     
-    
-    power_set(nums, index + 1, temp)
+    power_set(nums, index + 1, temp[:])
     temp.append(nums[index])
-    power_set(nums, index + 1, temp)
-    temp.pop() # why pop ? 
+    power_set(nums, index + 1, temp[:])
 
 
 # immutable objects in python are pass-by-value 
 # mutable objects in python are pass-by-refrence 
 
+power_set(nums, 0, [])
+print(storage)
