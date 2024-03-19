178d434c5c55a15bc3301243f3530f9c43fce87c
esohel30 <eric.sohel7@gmail.com>
2024-03-19 04:21:11 -0400

    updated

--- leetcode/powerset.py
+++ leetcode/powerset.py
@@ -6,15 +6,17 @@ storage = []
 def power_set(nums, index, temp):
     
     if index == len(nums):
-        storage.append(temp[:])
+        storage.append(temp[:]) # why copy ? 
         return 
     
     
     power_set(nums, index + 1, temp)
     temp.append(nums[index])
     power_set(nums, index + 1, temp)
-    temp.pop()
+    temp.pop() # why pop ? 
 
 
 power_set(nums, 0, [])
 print(storage)
+
+
