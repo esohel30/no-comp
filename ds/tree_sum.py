6da5db431fd7283c84e62becc3371ce6b1dff8c7
esohel30 <eric.sohel7@gmail.com>
2024-03-31 18:21:00 -0400

    added bfs solution to tree sum

---
+++ ds/tree_sum.py
@@ -0,0 +1,40 @@
+class Node: 
+    def __init__(self, val):
+        self.val = val 
+        self.left = None 
+        self.right = None 
+
+
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(2)
+f = Node(1)
+
+a.left = b 
+a.right = c 
+b.left = d 
+b.right = e 
+c.right = f 
+
+
+def tree_sum(root): # bfs iterative 
+    res = 0 
+    if root == None: 
+        return res 
+    
+    queue = [root]  
+    while queue: 
+        current = queue.pop()
+        res += current.val 
+
+        if current.left: 
+            queue.insert(0, current.left)
+        if current.right: 
+            queue.insert(0, current.right)
+
+    return res 
+
+print(tree_sum(a))
+
